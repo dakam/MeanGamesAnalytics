7ccbd7ad4aed847a90f2b517558ccf0ef0429607
Kato Damian <katodakam2010@gmail.com>
2021-07-15 15:44:36 -0600

    analytics upload removed angularjs

--- meanGamesAnalytica/public/angularjs-app/navigation-directive/navigation-directive.js
+++
@@ -1,8 +0,0 @@
-angular.module("meanGames").directive("gamesNavigation", GamesNavigation);
-
-function GamesNavigation() {
-    return {
-        restrict: "E",
-        templateUrl: "angularjs-app/navigation-directive/navigation-directive.html"
-    }
-}
